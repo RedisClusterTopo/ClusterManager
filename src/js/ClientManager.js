b2a29f1bc5dbcd0bf7006e111da0bf9925181ed0
alkief <akiefer6@gmail.com>
2016-11-26 18:12:46 -0500

    Finish parsing logic related to ec2 information

--- src/js/ClientManager.js
+++ src/js/ClientManager.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   var socket = io('http://localhost:8080');
 
   //Pass the key/value pair submitted to identify the client
-  clientID = JSON.parse(sessionStorage.id);
+  clientID = JSON.parse(localStorage.id);
   socket.emit('init app', clientID);
 
 
--- src/js/ClusterObjectParser.js
+++ src/js/ClusterObjectParser.js
@@ -5,10 +5,20 @@ function parse(topo_data, done){
 
   topo_data.ec2info.forEach(function(inst, i){
 
-    //TODO: Rework data structure to have all method calls coming from the
-        //TopoCluster object
-    //Build a parsed object to be forwarded to graphics generation
+    var az = new AvailabilityZone();
+    az.setName(inst.Placement.AvailabilityZone);
+    t.addAvailabilityZone(az);
 
+    var sn = new Subnet();
+    sn.setNetId(inst.SubnetId);
+    t.addSubnet(sn, az);
+
+    var i = new TopoInstance();
+    i.setId(inst.InstanceId);
+    i.setIp(inst.PrivateIpAddress);
+    t.addInstance(i, sn, az);
+
+    //TODO: Build and add node objects
   });
 
   done(t);
