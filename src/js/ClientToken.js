cefd46e28aa4b2e4622021f7504425f0767ed829
alkief <akiefer6@gmail.com>
2016-12-11 14:14:05 -0500

    Make indentation easier to read

--- src/js/ClientToken.js
+++ src/js/ClientToken.js
@@ -5,41 +5,41 @@ var querymanager = require('./query-manager.js');
 
 module.exports = class ClientToken {
 
-  constructor(k, v, socket){
-    this.client_id = {key : k, val : v};
-    this.socket = socket;
-    this.nodes = null;
-    this.cluster_commander = null;
-    this.ec2data = null;
-    this.queryManager = new querymanager();
-  }
+    constructor(k, v, socket){
+        this.client_id = {key : k, val : v};
+        this.socket = socket;
+        this.nodes = null;
+        this.cluster_commander = null;
+        this.ec2data = null;
+        this.queryManager = new querymanager();
+    }
 
 
-  queryEC2(cb){
-    this.queryManager.getInstancesByTag(this.client_id, function(d){
-      cb(d);
-    });
-  }
+    queryEC2(cb){
+        this.queryManager.getInstancesByTag(this.client_id, function(d){
+            cb(d);
+        });
+    }
 
-  parseNodes(){
-    //TODO: iterate through this.ec2data to build a hash/port map of cluster nodes
-      //possibly chain a call to update this.cluster_commander with new nodes?
+    parseNodes(){
+        //TODO: iterate through this.ec2data to build a hash/port map of cluster nodes
+        //possibly chain a call to update this.cluster_commander with new nodes?
 
-  }
+    }
 
-  initCommander(){
-    //TODO: implement ClusterCmdManager and set object instances to this.cluster_commander
-  }
+    initCommander(){
+        //TODO: implement ClusterCmdManager and set object instances to this.cluster_commander
+    }
 
-  setEC2Data(d){
-    this.ec2data = d;
-  }
+    setEC2Data(d){
+        this.ec2data = d;
+    }
 
-  getClientID(){
-    return this.client_id;
-  }
+    getClientID(){
+        return this.client_id;
+    }
 
-  getEC2Data(){
-    return this.ec2data;
-  }
+    getEC2Data(){
+        return this.ec2data;
+    }
 }
--- src/server.js
+++ src/server.js
@@ -4,14 +4,7 @@ var io = require('socket.io')(server);
 var express = require('express');
 var path = require('path');
 
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-west-2'});
-var ec2 = new AWS.EC2({apiVersion: '2016-09-15'});
-
-
 var Command = require('ioredis').Command;
-
-
 var ClientToken = require('./js/server-scripts/ClientToken.js');
 
 //Store objects containing ec2 info, ioredis info and the corresponding socket
@@ -24,90 +17,90 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'aws-login.html'));
+    res.sendFile(path.join(__dirname, '../public', 'aws-login.html'));
 });
 
 app.get('/index', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 
 //Add event listeners to new socket connections
 io.on('connection', function (socket) {
-  // User connects and wants to access with the tag given
-  socket.on('init-tag', function(tag){
+    // User connects and wants to access with the tag given
+    socket.on('init-tag', function(tag){
 
-    clientStore.forEach(function(c){
-      //TODO: Check for duplicates via tag ID
-    });
+        clientStore.forEach(function(c){
+            //TODO: Check for duplicates via tag ID
+        });
 
-    initClient(tag, socket, function(){
-      // Client is initialized and added to clientStore
-    });
-
-  });
-
-  //Initial forward of ec2 info to client
-  socket.on('init app', function(clientID){
-    //Find the associated client and emit the stored data without querying ec2
-    //No ioredis info is known here
-    clientStore.forEach(function(cl){
-      if(cl.getClientID().val == clientID.val && cl.getClientID().key == clientID.key){
-        socket.emit('topo init', cl.getEC2Data());
-      }
-    });
-  });
-
-  //Make calls to ec2, update cluster node states then forward data to client
-  socket.on('update topo', function(clientID){
-    updateClient(clientID, function(){
+        initClient(tag, socket, function(){
+            // Client is initialized and added to clientStore
+        });
 
     });
-  });
+
+    //Initial forward of ec2 info to client
+    socket.on('init app', function(clientID){
+        //Find the associated client and emit the stored data without querying ec2
+        //No ioredis info is known here
+        clientStore.forEach(function(cl){
+            if(cl.getClientID().val == clientID.val && cl.getClientID().key == clientID.key){
+                socket.emit('topo init', cl.getEC2Data());
+            }
+        });
+    });
+
+    //Make calls to ec2, update cluster node states then forward data to client
+    socket.on('update topo', function(clientID){
+        updateClient(clientID, function(){
+
+        });
+    });
 });
 
 function initClient(tag, socket, callback){
-  //Iterate through stored clients until a tag key/val is matched
-  clientStore.forEach(function(cl){
-    if(tag.key == cl.getClientID().key && tag.val == cl.getClientID().val){
-      socket.emit('topo init', cl.ec2data); //Send the stored ec2 data
-      return;//Don't init a new client token
-    }
-  });
+    //Iterate through stored clients until a tag key/val is matched
+    clientStore.forEach(function(cl){
+        if(tag.key == cl.getClientID().key && tag.val == cl.getClientID().val){
+            socket.emit('topo init', cl.ec2data); //Send the stored ec2 data
+            return;//Don't init a new client token
+        }
+    });
 
-  var newClient = new ClientToken(tag.key, tag.val, socket);
+    var newClient = new ClientToken(tag.key, tag.val, socket);
 
-  //Make ec2 query call and update token info based on return of query
-  newClient.queryEC2(function(d){
-    newClient.setEC2Data(d);
+    //Make ec2 query call and update token info based on return of query
+    newClient.queryEC2(function(d){
+        newClient.setEC2Data(d);
 
-    // newClient.parseNodes();   //Unimplemented - token storage of host/port map for nodes
-    // newClient.initCommander(); //Unimplemented - token storage of ioredis object
+        // newClient.parseNodes();   //Unimplemented - token storage of host/port map for nodes
+        // newClient.initCommander(); //Unimplemented - token storage of ioredis object
 
-    clientStore.push(newClient);
+        clientStore.push(newClient);
 
-    newClient.socket.emit('tag-response', null); //Tell client to advance to index
-  });
+        newClient.socket.emit('tag-response', null); //Tell client to advance to index
+    });
 }
 
 
 function updateClient(cid, callback){
-  //Iterate through stored clients until a tag key/val is matched
-  clientStore.forEach(function(cl){
-    if(tag.key == cl.getClientID().key && tag.val == cl.getClientID().val){
-      cl.queryEC2(function(d){
-        cl.setEC2Data(d);
-        // cl.parseNodes(); //Unimplemented
-        cl.socket.emit('topo update', cl.getEC2Data());
-      });
-    }
-  });
+    //Iterate through stored clients until a tag key/val is matched
+    clientStore.forEach(function(cl){
+        if(tag.key == cl.getClientID().key && tag.val == cl.getClientID().val){
+            cl.queryEC2(function(d){
+                cl.setEC2Data(d);
+                // cl.parseNodes(); //Unimplemented
+                cl.socket.emit('topo update', cl.getEC2Data());
+            });
+        }
+    });
 }
 
 
 var debugMode = true;
 function debug(s) {
-  if (debug){
-    console.log(s);
-  }
+    if (debug){
+        console.log(s);
+    }
 }
